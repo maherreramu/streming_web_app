d158c5de1509ec0b1bf4cdb1daeb5453e65fdd36
maherreramu <maherreramu@unal.edu.co>
2022-01-11 03:18:19 -0500

    Push project

---
+++ backend/mockData.js
@@ -0,0 +1,28 @@
+const allVideos = [
+    {
+        id: "v1",
+        poster: 'https://sn3302files.storage.live.com/y4mm1tNItNxov1IMT7mRJYVYYTaqpeQ-sr6KgKRy6Ri3giUuM0roN_Y-wjqDJPRiH153Kwh0eEYQskEzxJ6o32ZQ45pAfw6O5H42oR0mn94BK15onx0YmsNekiacZN10DINAPHzdWfI9Fk8ajKnUXDAFHnCMxfm02NY2bZ6g_xrx3WxP8SDFXwVaNJzvyD2ERza?width=1366&height=768&cropmode=none',
+        duration: '00:00:32',
+        name: 'v1'
+    },
+    {
+        id: "v2",
+        poster: 'https://sn3302files.storage.live.com/y4mFXMkmGx30FMVJnjigi-cC76An2Vnn9fMJYOPKBF1VtDCa5uQpDqXzrGuZ0Rb0kGmMkS4rzR8QkIs8H3trldrjSSE2c7sfRi5vJXEpHFCaKI1qccJptGmf8QqleKdq16Eyob1AebGhnscPejEx22HRGowIe15VPOG1bI2OAt3HraxMvvX1ff1jGDCX5uGSxS3?width=1366&height=768&cropmode=none',
+        duration: '00:00:07',
+        name: 'v2'
+    },
+    {
+        id: "v3",
+        poster: 'https://sn3302files.storage.live.com/y4mmO7coW6KHyinfyCVDnLP9g4qid1UkNeD7OsFXJItOvbWP5cM8tBDmMRmhFGZ0KPz-mucUbyNh6dAIoqME49oXn2FOCxV69DQSPzmnfJ4XZoWuhHw1ldkLUhrxDrSYQZaKCbHGeDIQf9Yc54YPYDnIJSNH0sIkIruGWNvOch77r6TPWWYIMTjENlzfQJQHkGf?width=1366&height=768&cropmode=none',
+        duration: '00:00:15',
+        name: 'v3'
+    },
+    {
+        id: "v4",
+        poster: 'https://sn3302files.storage.live.com/y4mZv-5VpIEOZUlOfMMUc7DTlb1At9_7tt6TQVX0uMzzv_MPUGxLjnrwiCxidAxbuKdXI0iT7KFhNdsFm7KD56oFrq02T2EeUIekYToutRSK2pEPdkXleMQ3tR6BUz2rgQCYGx2ewFo9AJ4NRchOj_hpVNYWuY2t-hr_-EdJN32pRn0NumfPqGC3Cff7vOHRfCF?width=1366&height=768&cropmode=none',
+        duration: '00:01:28',
+        name: 'v4'
+    }
+];
+
+module.exports = allVideos
